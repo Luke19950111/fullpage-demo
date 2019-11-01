9ae4dbe8b29fa26e12256b63b13da778cc76b0b8
liukuai <937613925@qq.com>
2019-11-01 18:04:29 +0800

    down过渡

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue/dist/vue.js'
+import { isRegExp } from 'util'
+// import App from './App'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+//   render: h => h()
+  data(){
+      return{
+          bgColor: ['#30c', '#c03', '#c30', '#3c0', '#03c'],
+          curIndex: 0
+      }
+  },
+  methods: {
+      onWheelScroll(e){
+          console.log(e, 'eee')
+          if(e.deltaY > 0){
+              this.curIndex ++
+          }
+      }
+  },
 })
